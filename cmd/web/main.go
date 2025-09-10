9f64325a832cdb8503561af236b55bfb8ffaad66
acdlee <christopher.d.lee123@gmail.com>
2025-09-10 10:19:36 -0400

    Add: CLI flags for port number

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-    mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-    mux.HandleFunc("GET /{$}", home)
-    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-    mux.HandleFunc("GET /snippet/create", snippetCreate)
-    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Print("starting server on :4000")
-    
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	// Build a hyperlink for dev
+	url := fmt.Sprintf("http://localhost%s", *addr)
+
+	log.Printf("Starting server on %s", *addr)
+	// Log the URL
+	log.Printf("Local:   \033[1;36m%s\033[0m\n", url)
+
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
